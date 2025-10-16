b69e02c50a585bdc4851e327ef8dffeb3aa73cc3
deepred5 <948931744@qq.com>
2025-10-16 17:23:27 +0800

    feat: demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,4 +4,13 @@ Page({
   data: {
     motto: 'Hello World',
   },
+  handleTap(event) {
+    const {
+      type
+    } = event.target.dataset
+
+    wx.navigateTo({
+      url: `../${type}/index`
+    })
+  },
 })
