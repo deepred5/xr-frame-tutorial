13172fd7b65501585f9504732f871a4ebe6c0208
deepred5 <948931744@qq.com>
2025-10-23 00:21:45 +0800

    feat: demo8

---
+++ pages/demo8/index.js
@@ -0,0 +1,90 @@
+// pages/demo8/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    left: 0,
+    top: 0,
+    width: 0,
+    height: 0,
+    renderWidth: 0,
+    renderHeight: 0,
+    hiddenTip: false,
+    markerImg: 'https://assets.papegames.com/resources/cdn/20251022/fa0b20289040dcd1.jpeg',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    const info = wx.getSystemInfoSync()
+    const width = info.windowWidth
+    const height = info.windowHeight
+    const dpi = info.pixelRatio
+
+
+    this.setData({
+      width, 
+      height,
+      renderWidth: width * dpi,
+      renderHeight: height * dpi
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  },
+
+  handleTrackerSwitch(cur) {
+    this.setData({
+      hiddenTip: cur.detail.active,
+    });
+  }
+})
